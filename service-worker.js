3e3ce27e55aeee9c548e9cbec5429c74db518264
TakahideSuzuki <t.suzuki@jssa.nl>
2025-05-06 02:42:04 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,7 @@
-self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('kintai-cache-v1').then((cache) => {
-      return cache.addAll([
-        './kintai.html',
-        './manifest.json',
-        './icon-192.png',
-        './icon-512.png'
-      ]);
-    })
-  );
+self.addEventListener('install', function (event) {
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+self.addEventListener('fetch', function (event) {
+  // デフォルトのフェッチ処理
 });
